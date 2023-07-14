3c72ac8755138f78848ef74b97d5994fc916b674
ACE9935 <anaselmouden99@gmail.com>
2023-07-14 12:35:59 +0200

    initial commit

---
+++ components/Box.js
@@ -0,0 +1,38 @@
+import { useEffect, useState,useRef } from "react";
+import Timer from "./Timer";
+import PlayBar from "./PlayBar";
+
+function Box() {
+
+    const [sx,setSx]=useState('The only way to do great work is to love what you do.')
+    const [win,setWin]=useState(false)
+    const [start,setStart]=useState(false)
+    const [reset,setReset]=useState(false)
+
+    const handleClick1 = event => {
+      setStart(true)
+    };
+
+    const handleClick2 = event => {
+      setWin(false)
+      setReset(true)
+    };
+
+    return ( 
+        <div className="bg-blue-500 w-[calc(100vw-20px)] max-w-[600px] aspect-[1.5/1] rounded-xl flex flex-col relative items-center">
+        <h1 className='font-bold text-3xl text-white text-center p-3 pb-16 bg-blue-700 rounded-t-xl w-full h-fit'>Type Racer</h1>
+        <div className='bg-white w-[80%] aspect-[1.5/1] mb-8 translate-y-[-10%] rounded-xl relative flex flex-col'>
+         <div className="grid place-items-center grow">
+      <Timer isPaused={win || !start} {...{reset,setReset}}/>
+         </div>
+           <div className='bg-blue-300 rounded-xl border-dashed border-2 border-black flex flex-col items-center gap-6 pt-8 pb-12 px-5'>
+          {start && !win && <PlayBar {...{win,setWin,sx}}/>}
+            {!start && <button onClick={handleClick1} className="text-xl bg-green-600 p-5 text-white font-bold hover:bg-green-700">Start Race!</button>}
+            {win && <button onClick={handleClick2} className="text-xl bg-green-600 p-5 text-white font-bold hover:bg-green-700">Play Again!</button>}
+           </div>
+        </div>
+       </div>
+     );
+}
+
+export default Box;
---
+++ components/Timer.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+
+function Timer({isPaused,reset,setReset}) {
+
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [pausedTime, setPausedTime] = useState(null);
+
+    useEffect(() => {
+        let interval;
+      
+        if(reset){
+            setMinutes(0)
+            setSeconds(0)
+            setReset(false)
+        }
+
+        if (!isPaused) {
+          interval = setInterval(() => {
+            if (seconds < 59) {
+              setSeconds((prevSeconds) => prevSeconds + 1);
+            } else {
+              setSeconds(0);
+              setMinutes((prevMinutes) => prevMinutes + 1);
+            }
+          }, 1000);
+        } else {
+          clearInterval(interval);
+        }
+      
+        return () => clearInterval(interval);
+      }, [isPaused, seconds, minutes]);
+
+    return ( 
+        <div className="text-6xl font-bold">
+    
+        {minutes.toString().padStart(2, '0')}:
+        {seconds.toString().padStart(2, '0')}
+      
+        </div>
+     );
+}
+
+export default Timer;
